332891ab7a7bc6d35f12fd6b4c681e4c32b8cf63
okalouti <kalouo@pm.me>
2019-02-26 20:40:39 +0900

    changes migration to accomodate carrier as argument

--- migrations/3_deploy_trade.js
+++ migrations/3_deploy_trade.js
@@ -2,6 +2,7 @@ const Trade = artifacts.require("Trade");
 const params = [
   "0x8ea35ac607d9183378062e73fe159d6846175c0e",
   "0x74fdcdc9d698008100cbffa7696a5e5b809ace0e",
+  "0x52273fdC9CD92c2777971Ea886b1AfCb6d40776E",
   200,
   1000
 ];
